99b10a1da98314cf11216dcc900e1873994cf28d
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2025-01-27 00:08:15 -0300

    initialize project with Next.js, Tailwind CSS, and essential configurations

---
+++ src/components/forms/login-form.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { ModeToggle } from "@/components/toggle/theme-toggle"
+
+export function LoginForm({
+  className,
+  ...props
+}: Readonly<React.ComponentPropsWithoutRef<"div">>) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">
+            <p>Login</p>
+              <ModeToggle />
+            </CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <button
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </button>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+              <Button variant="outline" className="w-full">
+                Login with Google
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <button className="underline underline-offset-4">
+                Sign up
+              </button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
